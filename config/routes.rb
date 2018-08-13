239c2e66eb54b651ea819b6e5edc335fa567aa8b
EC2 Default User <ec2-user@ip-172-31-9-54.us-east-2.compute.internal>
2018-08-13 09:50:45 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    root 'sweets#index'
   resources :sweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
